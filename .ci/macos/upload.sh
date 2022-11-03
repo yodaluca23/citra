3b9ed5234dbba74b49a8859081b5c886bf282a11
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2022-11-03 02:15:09 -0700

    ci: Fix macOS upload script calling wrong macpack.

--- .ci/macos/upload.sh
+++ .ci/macos/upload.sh
@@ -13,12 +13,12 @@ cp -r build/bin/Release/citra-qt.app "$REV_NAME"
 cp build/bin/Release/citra-room "$REV_NAME"
 
 # move libs into folder for deployment
-macpack "${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt" -d "../Frameworks"
+python3 -m macpack.patcher "${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt" -d "../Frameworks"
 # move qt frameworks into app bundle for deployment
 $(brew --prefix)/opt/qt5/bin/macdeployqt "${REV_NAME}/citra-qt.app" -executable="${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt"
 
 # move libs into folder for deployment
-macpack "${REV_NAME}/citra" -d "libs"
+python3 -m macpack.patcher "${REV_NAME}/citra" -d "libs"
 
 # workaround for libc++
 install_name_tool -change @loader_path/../Frameworks/libc++.1.0.dylib /usr/lib/libc++.1.dylib "${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt"
